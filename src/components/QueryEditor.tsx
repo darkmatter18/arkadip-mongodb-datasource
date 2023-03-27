83bcdcbe382f7d1bedf3503f047e5cfccd7f0dc9
Arkadip <hi@arkadip.dev>
2023-03-27 21:55:41 +0530

    feat: updated row count in the queryeditor

--- src/components/QueryEditor.tsx
+++ src/components/QueryEditor.tsx
@@ -8,31 +8,12 @@ import { MyDataSourceOptions, MyQuery } from '../types';
 type Props = QueryEditorProps<DataSource, MyQuery, MyDataSourceOptions>;
 
 export function QueryEditor({ query, onChange, onRunQuery }: Props) {
-  // const onTimeField = (event: ChangeEvent<HTMLInputElement>) => {
-  //   onChange({ ...query, time_field: event.target.value });
-  // };
-
-  // const onDatabaseChange = (event: ChangeEvent<HTMLInputElement>) => {
-  //   onChange({ ...query, database: event.target.value });
-  //   // executes the query
-  //   // onRunQuery();
-  // };
-
-  // const onCollectionChange = (event: ChangeEvent<HTMLInputElement>) => {
-  //   onChange({ ...query, collection: event.target.value });
-  //   // executes the query
-  //   // onRunQuery();
-  // };
-
-
    const onQueryChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     onChange({ ...query, q: event.target.value });
     // executes the query
     // onRunQuery();
   };
   
-
-  // const { time_field, database, collection } = query;
   const { q } = query;
 
   return (
@@ -40,17 +21,7 @@ export function QueryEditor({ query, onChange, onRunQuery }: Props) {
       <InlineLabel width="auto" tooltip="Database Query">
         Query
       </InlineLabel>
-      <TextArea onChange={onQueryChange} value={q} />
-
-      {/* <InlineField label="Database" tooltip="Name of the Database">
-        <Input onChange={onDatabaseChange} value={database} width={16} type="string" />
-      </InlineField>
-      <InlineField label="Collection" tooltip="Name of the collection">
-        <Input onChange={onCollectionChange} value={collection} width={20} type="string" />
-      </InlineField>
-      <InlineField label="Time field" tooltip="The time field">
-        <Input onChange={onTimeField} value={time_field || ''} />
-      </InlineField> */}
+      <TextArea onChange={onQueryChange} value={q} rows={10} />
     </div>
   );
 }
